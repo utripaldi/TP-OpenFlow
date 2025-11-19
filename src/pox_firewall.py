939e6a682551b391368514bcb030fa20652659d1
IgnacioAdelardi12 <ignacioadelardi@gmail.com>
2025-11-19 19:59:39 -0300

    init actualizado

--- src/pox_firewall.py
+++ src/pox_firewall.py
@@ -1,3 +1,4 @@
+import json
 from pox.core import core
 import pox.openflow.libopenflow_01 as of
 from pox.lib.revent import *
@@ -11,6 +12,19 @@ log = core.getLogger()
 class Firewall(EventMixin):
     def __init__(self):
         self.listenTo(core.openflow)
+        log.debug("Enabling Firewall Module")
+    
+    def __init__(self):
+        # Load rules
+        with open("firewall_rules.json", "r") as f:
+            self.config = json.load(f)
+
+        self.selected_switches = self.config.get("selected_switches", [])
+        self.rules = self.config.get("rules", [])
+
+        # Register for OpenFlow events
+        self.listenTo(core.openflow)
+
         log.debug("Enabling Firewall Module")
 
     def _handle_ConnectionUp(self, event):
